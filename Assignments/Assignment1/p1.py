cbd193ea8d8627609ba0ccccefe8eac1c304cab8
Raymond Christopher <raymond@delman.io>
2022-08-21 04:42:24 +0000

    Assignment 1 (initial) + autograder

---
+++ Assignments/Assignment1/p1.py
@@ -0,0 +1,25 @@
+"""
+Divide it whole.
+
+Given two non-negative integers a and b, find the result of dividing a with b but with some notes:
+- If the result is not an integer, return the result rounded down to the nearest integer.
+- If b equals 0, return -1
+
+Examples
+------------------------------------
+Input: a = 13, b = 3
+Output: 4
+Explanation: 13/3 = 4.3333.., rounded down to 4
+------------------------------------
+Input: a = 4, b = 9
+Output: 0
+Explanation: 4/9 = 0.444.., rounded down to 0
+------------------------------------
+Input: a = 7, b = 0
+Output: -1
+------------------------------------
+"""
+
+
+def divide_whole(a: int, b: int) -> int:
+    pass
---
+++ Assignments/Assignment1/p3.py
@@ -0,0 +1,23 @@
+"""
+Even sum.
+
+Given a list of integers, return the sum of the even numbers within a list. 
+If the list doesn't contain any even numbers, return 0. The list will contain
+at most 1000 integers between -1000 and 1000 (inclusive).
+
+Examples
+------------------------------------
+Input: [0, 2, 3, 4]
+Output: 6
+Explanation: All numbers are even except 3, so the total is 0+2+4 = 6
+------------------------------------
+Input: [-11, 3, 7 ]
+Output: 0
+Explanation: All numbers are odd, so return 0
+------------------------------------
+
+"""
+
+
+def even_sum(l: list):
+    pass

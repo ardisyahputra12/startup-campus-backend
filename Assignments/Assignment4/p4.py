b0e7be88f2fa4f1906a08b090082798845ff39d4
Ardi Syahputra <ardisaputraa12@gmail.com>
2022-10-04 12:03:50 +0700

    P4 of Assignment 4 Completed

--- Assignments/Assignment4/p4.py
+++ Assignments/Assignment4/p4.py
@@ -93,7 +93,17 @@ by a unique <name>.
     - return {"error": "You never borrow book <title>"}
     - status code: 400
 """
-from flask import Flask
+import os
+from flask import Flask, request
+from sqlalchemy import Column, MetaData, String, Table, create_engine, text
+
+db_name = "p4.db"
+if os.path.isfile(db_name):
+    os.remove(db_name)
+
+def get_engine():
+    """Creating SQLite Engine to interact"""
+    return create_engine(f"sqlite:///{db_name}", future=True)
 
 
 def create_app():
@@ -102,7 +112,21 @@ def create_app():
     # IMPLEMENT THESE:
     # - create a SQLite database
     # - create all required tables to store information regarding books and users
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    engine = get_engine()
+    meta = MetaData()
+    Table(
+        "books",
+        meta,
+        Column("title", String, nullable=False, unique=True)
+    )
+    Table(
+        "visitors",
+        meta,
+        Column("name", String, nullable=False),
+        Column("title", String, nullable=False, unique=True)
+    )
+    meta.create_all(engine)
 
     return app
 
@@ -110,9 +134,104 @@ def create_app():
 app = create_app()
 
 
+def error_message(msg: str, sts: int):
+    return {
+        "error": msg
+    }, sts
+
+def success_message(msg: str, sts: int):
+    return {
+        "message": msg
+    }, sts
+
+@app.route("/book", methods=["POST"])
+def add_new_book():
+    data = request.get_json()
+    # Request body:
+    #     - title: string (required)
+    books_title = run_query(f"SELECT title FROM books WHERE title = '{data['title']}'")
+
+    if [{"title": data["title"]}] == books_title:
+        return error_message("Book with the same title already exists", 400)
+    else:
+        run_query(f"INSERT INTO books VALUES ('{data['title']}')", commit=True)
+        return success_message(f"Book {data['title']} is added", 201)
+
+@app.route("/book", methods=["DELETE"])
+def remove_book():
+    data = request.get_json()
+    # Request body:
+    #     - title: string (required)
+    visitors_name = run_query(f"SELECT name FROM visitors WHERE title = '{data['title']}'")
+    visitors_title = run_query(f"SELECT title FROM visitors WHERE title = '{data['title']}'")
+    books_title = run_query(f"SELECT title FROM books WHERE title = '{data['title']}'")
+
+    if [{"title": data["title"]}] != books_title:
+        return error_message("Book is not known", 400)
+    elif [{"title": data["title"]}] == visitors_title:
+        return error_message(f"Book is currently borrowed by {visitors_name[0]['name']}", 403)
+    else:
+        run_query(f"DELETE FROM books WHERE title='{data['title']}'", commit=True)
+        return success_message(f"Book {data['title']} is removed", 200)
+
+
+@app.route("/borrow", methods=["POST"])
+def borrow_book():
+    data = request.get_json()
+    # Request body:
+    #     - name: name of the visitor (required)
+    #     - title: string (required)
+    books_title = run_query(f"SELECT title FROM books WHERE title = '{data['title']}'")
+    visitors_title_name = run_query(f"SELECT title FROM visitors WHERE title = '{data['title']}' AND name = '{data['name']}'")
+    visitors_title = run_query(f"SELECT title FROM visitors WHERE title = '{data['title']}'")
+
+    if [{"title": data["title"]}] != books_title:
+        return error_message("Book is not known", 400)
+    elif [{"title": data["title"]}] == visitors_title_name:
+        return error_message("You are currently borrowing this book", 400)
+    elif [{"title": data["title"]}] == visitors_title:
+        return error_message("Book is currently borrowed", 403)
+    else:
+        run_query(f"INSERT INTO visitors VALUES {data['name'], data['title']}", commit=True)
+        return success_message(f"Book {data['title']} is borrowed by {data['name']}", 200)
+
+@app.route("/return", methods=["POST"])
+def return_book():
+    data = request.get_json()
+    # Request body:
+    #     - name: name of the visitor (required)
+    #     - title: string (required)
+    visitors_name = run_query(f"SELECT name FROM visitors WHERE title = '{data['title']}'")
+    visitors_title = run_query(f"SELECT title FROM visitors WHERE title = '{data['title']}'")
+
+    if [{"name": data["name"]}] != visitors_name:
+        return error_message(f"You never borrow book {visitors_title[0]['title']}", 400)
+    else:
+        run_query(f"DELETE FROM visitors WHERE title='{data['title']}'", commit=True)
+        return success_message(f"Book {visitors_title[0]['title']} is returned safely", 200)
+
+
 ##############################################################################################
 # Helper Methods
 ##############################################################################################
+
+
+def run_query(query, commit: bool = False):
+    """Runs a query against the given SQLite database.
+
+    Args:
+        commit: if True, commit any data-modification query (INSERT, UPDATE, DELETE)
+    """
+    engine = get_engine()
+    if isinstance(query, str):
+        query = text(query)
+
+    with engine.connect() as conn:
+        if commit:
+            conn.execute(query)
+            conn.commit()
+        else:
+            return [dict(row) for row in conn.execute(query)]
 
 
 # TEST IT YOURSELF
